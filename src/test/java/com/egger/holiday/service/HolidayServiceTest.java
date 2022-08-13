d92e8d962909e993cf77f68ea5324648df1adcf8
Junior Egger <junior.egger@gmail.com>
2022-08-12 23:48:14 -0300

    (Em desenv)

---
+++ src/test/java/com/egger/holiday/service/HolidayServiceTest.java
@@ -0,0 +1,54 @@
+package com.egger.holiday.service;
+
+import com.egger.holiday.feign.HolidayFeignClient;
+import com.egger.holiday.mapper.HolidayMapper;
+import com.egger.holiday.model.response.Holiday;
+import com.egger.holiday.model.response.HolidayResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class HolidayServiceTest {
+
+    @Mock
+    private HolidayFeignClient holidayFeignClient;
+    @Mock
+    private HolidayMapper holidayMapper;
+    @InjectMocks
+    private HolidayService holidayService;
+
+    @Test
+    void shouldReturnHolidaysWithYear() {
+        //Given
+        String year = "2022";
+        List<Holiday> holidays = mockListHoliday();
+
+        //When
+        when(holidayFeignClient.getHolidaysByYear(year)).thenReturn(holidays);
+        List<HolidayResponse> actual = holidayService.getHolidaysByYear(year);
+
+        //Then
+        assertEquals("2022-01-01", actual.get(0).getDate(), "Deve retornar a mesma data");
+        assertEquals("Confraternização mundial", actual.get(0).getName(), "Deve retornar o mesmo nome do feriado");
+    }
+
+    private List<Holiday> mockListHoliday() {
+        Holiday holiday = new Holiday();
+        holiday.setDate("2022-01-01");
+        holiday.setName("Confraternização mundial");
+        holiday.setType("national");
+        List<Holiday> holidayList = new ArrayList<>();
+        holidayList.add(holiday);
+        return holidayList;
+    }
+
+}
